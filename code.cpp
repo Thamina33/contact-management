d7a81f06b8e6637383e89e76f0a298b674dc4f0e
rahat14 <rahatshovo6666@gmail.com>
2018-04-22 23:39:17 +0600

    again

--- code.cpp
+++ code.cpp
@@ -1,35 +1,35 @@
 #include <iostream>
 #include <fstream>
-#include  <string.h>
+#include <string.h>
 using namespace std;
 
-void delete_student()
+void delete_contact()
 {
 
     fstream stud;
     fstream temp;
 
-    stud.open("student.txt",ios::in);
+    stud.open("contact.txt",ios::in);
     temp.open("temp.txt",ios::out);
     char name [25];
-    char age[25];
+    char mail_add[25];
     char phone [25];
     char a[25];
     cin.ignore();
-    cout<<" \n\t enter the phone number to delete record : ";
+    cout<<" \n\t enter the name  number to delete record : ";
     cin.getline(a,25);
     while(!stud.eof())
     {
         stud.getline(name,25,'|');
         stud.getline(phone,25,'|');
-        stud.getline(age,25);
-        if(strcmp(phone,a)==0)
+        stud.getline(mail_add,25);
+        if(strcmp(name,a)==0)
         {
             continue;
         }
         else
         {
-            temp<< name<<'|'<<phone<<'|'<<age<<'\n';
+            temp<< name<<'|'<<phone<<'|'<<mail_add<<'\n';
         }
 
 
@@ -37,40 +37,40 @@ void delete_student()
     temp.close();
     stud.close();
 
-    stud.open("student.txt",ios::out);
+    stud.open("contact.txt",ios::out);
     temp.open("temp.txt",ios::in);
     while(!temp.eof())
     {
         temp.getline(name,25,'|');
         temp.getline(phone,25,'|');
-        temp.getline(age,25);
-        stud<< name<<'|'<<phone<<'|'<<age<<'\n';
+        temp.getline(mail_add,25);
+        stud<< name<<'|'<<phone<<'|'<<mail_add<<'\n';
     }
     temp.close();
     stud.close();
     remove("temp.txt");
     cout<<"\n done !!! \n";
 }
-void update_student()
+void update_contact()
 {
     fstream stud;
     fstream temp;
 
-    stud.open("student.txt",ios::in);
+    stud.open("contact.txt",ios::in);
     temp.open("temp.txt",ios::out);
     char name [25];
-    char age[25];
+    char mail_add[25];
     char phone [25];
     char a[25];
     cin.ignore();
-    cout<<" \n\t enter the phone number to update record : ";
+    cout<<" \n\t enter the name  number to update record : ";
     cin.getline(a,25);
     while(!stud.eof())
     {
         stud.getline(name,25,'|');
         stud.getline(phone,25,'|');
-        stud.getline(age,25);
-        if(strcmp(phone,a)==0)
+        stud.getline(mail_add,25);
+        if(strcmp(name,a)==0)
         {
 
             cout<<" \n\tenter new value of record  \n ";
@@ -79,12 +79,12 @@ void update_student()
             cout<<" \n phone : ";
             cin.getline(phone,25);
             cout<<" \n age : ";
-            cin.getline(age,25);
-            temp<< name<<'|'<<phone<<'|'<<age<<'\n';
+            cin.getline(mail_add,25);
+            temp<< name<<'|'<<phone<<'|'<<mail_add<<'\n';
         }
         else
         {
-            temp<< name<<'|'<<phone<<'|'<<age<<'\n';
+            temp<< name<<'|'<<phone<<'|'<<mail_add<<'\n';
         }
 
 
@@ -92,14 +92,14 @@ void update_student()
     temp.close();
     stud.close();
 
-    stud.open("student.txt",ios::out);
+    stud.open("contact.txt",ios::out);
     temp.open("temp.txt",ios::in);
     while(!temp.eof())
     {
         temp.getline(name,25,'|');
         temp.getline(phone,25,'|');
-        temp.getline(age,25);
-        stud<< name<<'|'<<phone<<'|'<<age<<'\n';
+        temp.getline(mail_add,25);
+        stud<< name<<'|'<<phone<<'|'<<mail_add<<'\n';
     }
     temp.close();
     stud.close();
@@ -109,44 +109,44 @@ void update_student()
 void display_all()
 {
     char name [25];
-    char age[25];
+    char mail_add[25];
     char phone [25];
 
     fstream stud;
-    stud.open("student.txt",ios::in);
+    stud.open("contact.txt",ios::in);
 
-    cout<< " \n\t name \t phone \t age \n";
+    cout<< " \n\t name \t phone \t Mail Address \n";
     while(!stud.eof())
     {
         stud.getline(name,25,'|');
         stud.getline(phone,25,'|');
-        stud.getline(age,25);
-        cout<< "\n \t "<< name << "\t"<<phone<<"\t"<<age<<endl;
+        stud.getline(mail_add,25);
+        cout<< "\n \t "<< name << "\t"<<phone<<"\t"<<mail_add<<endl;
 
     }
 }
-void search_student(   )
+void search_contact(   )
 {
 
     fstream stud;
-    stud.open("student.txt",ios::in);
+    stud.open("contact.txt",ios::in);
     char name [25];
-    char age[25];
+    char mail_add[25];
     char phone [25];
     char a[25];
-    cout<<" \n enter the phone to search about it : ";
+    cout<<" \n enter the name to search about it : ";
     cin.ignore();
     cin.getline(a,25);
     int x=0;
-    cout<< " \n\t name \t phone \t age \n";
+    cout<< " \n\t name \t phone \t mail address \n";
     while(!stud.eof())
     {
         stud.getline(name,25,'|');
         stud.getline(phone,25,'|');
-        stud.getline(age,25);
-        if(strcmp(phone, a)==0)
+        stud.getline(mail_add,25);
+        if(strcmp(name, a)==0)
         {
-            cout<< "\n \t "<< name << "\t"<<phone<<"\t"<<age<<endl;
+            cout<< "\n \t "<< name << "\t"<<phone<<"\t"<<mail_add<<endl;
             x=1;
             break;
         }
@@ -158,22 +158,22 @@ void search_student(   )
     }
     stud.close();
 }
-void  insert_student()
+void  insert_contact()
 {
 
     char name [25];
-    char age[25];
+    char mail_add[25];
     char phone [25];
     fstream stud;
-    stud.open("student.txt", ios::app);
+    stud.open("contact.txt", ios::app);
     cin.ignore();
     cout<<" \n name : ";
     cin.getline(name,25);
     cout<<" \n phone : ";
     cin.getline(phone,25);
-    cout<<" \n age : ";
-    cin.getline(age,25);
-    stud<< name<<'|'<<phone<<'|'<<age<<'\n';
+    cout<<" \n Mail Address : ";
+    cin.getline(mail_add,25);
+    stud<< name<<'|'<<phone<<'|'<<mail_add<<'\n';
     cout<< "done !";
     stud.close();
 }
@@ -189,20 +189,20 @@ int main()
         switch (c)
         {
         case 1 :
-            insert_student();
+            insert_contact();
             break;
         case 2:
-            search_student();
+            search_contact();
             break;
         case 3:
             display_all();
             break;
         case 4 :
-            update_student();
+            update_contact();
 
             break;
         case 5 :
-            delete_student();
+            delete_contact();
             break;
         default :
             cout << " \n choose right number \n";
